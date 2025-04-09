ed566a4ea2a619af9e11b565a7d1f7f60bb1d53a
Vitor Mina <148635045+vitorhmina@users.noreply.github.com>
2025-04-09 20:23:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# subscription-tracker-api
+ 
---
+++ routes/user.routes.js
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+
+import authorize from '../middlewares/auth.middleware.js'
+import { getUser, getUsers } from '../controllers/user.controller.js'
+
+const userRouter = Router();
+
+userRouter.get('/', getUsers);
+
+userRouter.get('/:id', authorize, getUser);
+
+userRouter.post('/', (req, res) => res.send({ title: 'CREATE new user' }));
+
+userRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE user' }));
+
+userRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE user' }));
+
+export default userRouter;
